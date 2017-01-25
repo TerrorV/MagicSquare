59a663f2af81a8ce48a8a62b17094acb98c83f66
Vladimir Ivanov <vladimir.ivanov@concep.com>
2017-01-25 18:56:59 +0200

    Initial commit

---
+++ MagicSquares.Tests/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MagicSquares.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCalculateMagicConstant()
+        {
+            int squareSize = 3;
+            int magicConstant = CalculateMagicConstant(squareSize);
+            var magicSquare = new int[squareSize];
+
+            Assert.AreEqual(magicConstant, 15);
+            Assert.AreEqual(CalculateMagicConstant(2), 5);
+            Assert.AreEqual(CalculateMagicConstant(4), 34);
+            Assert.AreEqual(CalculateMagicConstant(4), 34);
+            Assert.AreEqual(CalculateMagicConstant(122001), 977287841);
+            Assert.AreEqual(CalculateMagicConstant(122001), 977287841);
+
+        }
+
+        [TestMethod]
+        public void TestGenerateSquare()
+        {
+            int squareSize = 3;
+            int[] magicSquare = TestGenerateSquare(squareSize);
+
+        }
+
+        private int[] TestGenerateSquare(int squareSize)
+        {
+            var squareArray = new int[squareSize];
+            var magicConstant = CalculateMagicConstant(squareSize);
+            for (int i = 0; i < magicConstant; i++)
+            {
+
+            }
+
+            throw new NotImplementedException();
+        }
+
+        private int CalculateMagicConstant(int squareSize)
+        {
+            return squareSize * (squareSize * squareSize + 1) / 2;
+        }
+    }
+}
