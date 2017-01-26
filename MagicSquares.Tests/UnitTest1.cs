b32ee02c57dba2e8832abc83491de1eaa72fba80
Vladimir Ivanov <terrorv@gmail.com>
2017-01-26 09:32:20 +0200

    Initial attempt to implement the algorithm

--- MagicSquares.Tests/UnitTest1.cs
+++ MagicSquares.Tests/UnitTest1.cs
@@ -26,20 +26,137 @@ namespace MagicSquares.Tests
         public void TestGenerateSquare()
         {
             int squareSize = 3;
-            int[] magicSquare = TestGenerateSquare(squareSize);
+            int[,] magicSquare = GenerateOddSquare(squareSize);
 
         }
 
-        private int[] TestGenerateSquare(int squareSize)
+        private int[,] GenerateOddSquare(int squareSize)
         {
-            var squareArray = new int[squareSize];
+            var squareArray = new int[squareSize, squareSize];
             var magicConstant = CalculateMagicConstant(squareSize);
-            for (int i = 0; i < magicConstant; i++)
+            var x = squareSize - 1;
+            var y = (squareSize - 1) / 2;
+            ////var x = 3-squareSize;
+            ////var y = (squareSize - 1) / 2;
+            for (int i = 1; i <= squareSize*squareSize; i++)
             {
+
+                squareArray[x, y] = i;
+
+                //x++;
+                //y++;
+                if (x + 1 < squareSize && y + 1 < squareSize)
+                {
+                    if (squareArray[x + 1, y + 1] > 0)
+                    {
+                        //x--;
+                        //y--;
+                        x--;
+                    }
+                    else
+                    {
+                        x++;
+                        y++;
+
+                    }
+                }
+                else
+                {
+                    if (x + 1== squareSize && y + 1== squareSize)
+                    {
+                        if (squareArray[0, 0] > 0)
+                        {
+                            x--;
+                        }
+                        else
+                        {
+                            x = 0;
+                            y = 0;
+                        }
+
+                    }
+                    else if(x + 1 == squareSize)
+                    {
+                        if (squareArray[0, y + 1] > 0)
+                        {
+                            x--;
+                        }
+                        else
+                        {
+                            x = 0;
+                        }
+                    }
+                    else if(y + 1 == squareSize)
+                    {
+                        if (squareArray[x + 1, 0] > 0)
+                        {
+                            x--;
+                        }
+                        else
+                        {
+                            y = 0;
+                        }
+
+                    }
+                }
+
+
+
+
+
+                ////int coordX = x;
+                ////int coordY = y;
+                ////if(coordX<0)
+                ////{
+                ////    coordX += squareSize;
+                ////}
+
+                ////if (coordY < 0)
+                ////{
+                ////    coordY += squareSize;
+                ////}
+
+                ////if (coordX >= squareSize )
+                ////{
+                ////    coordX -= squareSize;
+                ////}
+
+                ////if (coordY >= squareSize)
+                ////{
+                ////    coordY -= squareSize;
+                ////}
+
+                ////squareArray[coordX, coordY] = i;
+
+                ////x++;
+                ////y--;
+
+
+                ////if (x < 0)
+                ////{
+                ////    x += squareSize;
+                ////}
+
+                ////if (coordY < 0)
+                ////{
+                ////    coordY += squareSize;
+                ////}
+
+                ////if (x >= squareSize)
+                ////{
+                ////    x -= squareSize;
+                ////}
+
+                ////if (coordY >= squareSize)
+                ////{
+                ////    coordY -= squareSize;
+                ////}
+
+
 
             }
 
-            throw new NotImplementedException();
+            return squareArray;
         }
 
         private int CalculateMagicConstant(int squareSize)
