620690bbfb0533a6496161f7f16588efabbf5051
Vladimir Ivanov <terrorv@gmail.com>
2017-01-26 09:37:32 +0200

    Rename to fit the conventions

--- MagicSquares.BL/Properties/AssemblyInfo.cs
+++ MagicSquares.BL/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MagicSquare.BL")]
+[assembly: AssemblyTitle("MagicSquares.BL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MagicSquare.BL")]
+[assembly: AssemblyProduct("MagicSquares.BL")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
