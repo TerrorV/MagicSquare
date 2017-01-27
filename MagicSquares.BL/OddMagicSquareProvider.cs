d8d7d22b24940f278055ad6c07fe38b7c6459bf9
Vladimir Ivanov <terrorv@gmail.com>
2017-01-28 01:30:29 +0200

    Odd Square - no array required

--- MagicSquares.BL/OddMagicSquareProvider.cs
+++ MagicSquares.BL/OddMagicSquareProvider.cs
@@ -14,26 +14,24 @@ namespace MagicSquares.BL
             var squareArray = new int[squareSize, squareSize];
             var x = squareSize - 1;
             var y = (squareSize - 1) / 2;
+            int iterator = 0;
             for (int i = 1; i <= squareSize * squareSize; i++)
             {
                 squareArray[x, y] = i;
-
                 var isRightMost = x + 1 == squareSize;
                 var isTopMost = y + 1 == squareSize;
 
-                // Advance one step
-                x = isRightMost ? 0 : x + 1;
-                y = isTopMost ? 0 : y + 1;
-
-                var isBusy = squareArray[x, y] > 0;
-                if(isBusy)
+                iterator++;
+                if (iterator == squareSize)
                 {
-                    // Back one step
-                    x = isRightMost ? squareSize - 1 : x-1;
-                    y = isTopMost ? squareSize - 1 : y-1;
-
-                    // Change track
+                    iterator = 0;
                     x--;
+                }
+                else
+                {
+                    // Advance one step
+                    x = isRightMost ? 0 : x + 1;
+                    y = isTopMost ? 0 : y + 1;
                 }
             }
 
