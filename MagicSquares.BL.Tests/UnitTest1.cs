00732b1f89b5a9b216de683d1803930e73debd29
Vladimir Ivanov <terrorv@gmail.com>
2017-01-27 18:29:20 +0200

    Implement Even Square

--- MagicSquares.BL.Tests/UnitTest1.cs
+++ MagicSquares.BL.Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MagicSquares.BL;
+using System.Diagnostics;
 
 namespace MagicSquares.BL.Tests
 {
@@ -8,18 +9,62 @@ namespace MagicSquares.BL.Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestGenerateEvenSquare()
         {
+            int squareSize = 314;
+            var magicConstantProvider = new MagicConstantProvider();
+            long magicConstant = new MagicConstantProvider().CalculateMagicConstant(squareSize);
+            Assert.IsTrue(magicConstant > 0);
+            int[,] magicSquare = new EvenMagicSquareProvider(new OddMagicSquareProvider()).GenerateMagicSquare(squareSize);
+
+            PrintSquare(magicSquare);
+            long diagSum1 = 0;
+            long diagSum2 = 0;
+            for (int i = 0; i < squareSize; i++)
+            {
+                long rowSum = 0;
+                long colSum = 0;
+                diagSum1 += magicSquare[i, i];
+                diagSum2 += magicSquare[i, squareSize - i - 1];
+
+                for (int j = 0; j < squareSize; j++)
+                {
+                    rowSum += magicSquare[i, j];
+                    colSum += magicSquare[j, i];
+                }
+
+                Assert.AreEqual(magicConstant, rowSum);
+                Assert.AreEqual(magicConstant, colSum);
+            }
+
+            Assert.AreEqual(magicConstant, diagSum1);
+            Assert.AreEqual(magicConstant, diagSum2);
+
+
+        }
+
+        private void PrintSquare(int[,] magicSquare)
+        {
+            var squareSize = magicSquare.GetLength(0);
+            for (int i = 0; i < squareSize; i++)
+            {
+                for (int j = 0; j < squareSize; j++)
+                {
+                    Debug.Write($"{magicSquare[i, j]} \t");
+                }
+
+                Debug.WriteLine(string.Empty);
+            }
         }
 
         [TestMethod]
-        public void TestGenerateEvenSquare()
+        public void TestGenerateDoublyEvenSquare()
         {
             int squareSize = 1000;
             var magicConstantProvider = new MagicConstantProvider();
             long magicConstant = new MagicConstantProvider().CalculateMagicConstant(squareSize);
             Assert.IsTrue(magicConstant > 0);
-            int[,] magicSquare = new DoublyEvenMagicSquareProvider(magicConstantProvider).GenerateMagicSquare(squareSize);
+            int[,] magicSquare = new DoublyEvenMagicSquareProvider().GenerateMagicSquare(squareSize);
 
             long diagSum1 = 0;
             long diagSum2 = 0;
--- MagicSquares.BL/EvenMagicSquareProvider.cs
+++ MagicSquares.BL/EvenMagicSquareProvider.cs
@@ -9,15 +9,57 @@ namespace MagicSquares.BL
 {
     public class EvenMagicSquareProvider : ISquareProvider
     {
-        IMagicConstantProvider _magicConstantProvider;
-        public EvenMagicSquareProvider(IMagicConstantProvider magicConstantProvider)
+        OddMagicSquareProvider _oddSquareProvider;
+        public EvenMagicSquareProvider(OddMagicSquareProvider oddSquareProvider)
         {
-            _magicConstantProvider = magicConstantProvider;
+            _oddSquareProvider = oddSquareProvider;
         }
 
         public int[,] GenerateMagicSquare(int squareSize)
         {
-            throw new NotImplementedException();
+            var halfSize = squareSize / 2;
+            var baseSquare = _oddSquareProvider.GenerateMagicSquare(halfSize);
+            var squareArray = new int[squareSize, squareSize];
+            for (int x = 0; x < halfSize; x++)
+            {
+                for (int y = 0; y < halfSize; y++)
+                {
+                    squareArray[x, y] = baseSquare[halfSize - x - 1, y];
+                    squareArray[x + halfSize, y + halfSize] = baseSquare[halfSize - x - 1, y] + squareSize * squareSize / 4;
+                    squareArray[x, y + halfSize] = baseSquare[halfSize - x - 1, y] + squareSize * squareSize / 2;
+                    squareArray[x + halfSize, y] = baseSquare[halfSize - x - 1, y] + 3 * squareSize * squareSize / 4;
+
+                }
+            }
+
+            TransformRawSquare(squareArray);
+
+            return squareArray;
+        }
+
+        private void TransformRawSquare(int[,] squareArray)
+        {
+            var squareSize = squareArray.GetLength(0);
+            var halfSize = squareSize / 2;
+            var shiftSize = (halfSize - 1) / 2 - 1;
+
+            for (int x = 0; x < halfSize; x++)
+            {
+                var addition = (x == ((halfSize - 1) / 2)) ? 1 : 0;
+                for (int y = 0; y < shiftSize + 1; y++)
+                {
+                    var tempItem = squareArray[x, y + addition];
+                    squareArray[x, y + addition] = squareArray[x + halfSize, y + addition];
+                    squareArray[x + halfSize, y + addition] = tempItem;
+                }
+
+                for (int y = 0; y < shiftSize; y++)
+                {
+                    var tempItem = squareArray[x, squareSize - y - 1];
+                    squareArray[x, squareSize - y - 1] = squareArray[x + halfSize, squareSize - y - 1];
+                    squareArray[x + halfSize, squareSize - y - 1] = tempItem;
+                }
+            }
         }
     }
 }
