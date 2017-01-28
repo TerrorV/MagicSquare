87eebf75718790877d963b49644243ce91fa11e9
Vladimir Ivanov <terrorv@gmail.com>
2017-01-28 11:01:46 +0200

    Unit test refactoring

--- MagicSquares.BL.Tests/UnitTest1.cs
+++ MagicSquares.BL.Tests/UnitTest1.cs
@@ -128,33 +128,4 @@ namespace MagicSquares.BL.Tests
 
         }
     }
-
-    [TestClass]
-    public class When_generating_Magic_Square_constant
-    {
-        [TestMethod]
-        public void Should_generate_a_positive_constant()
-        {
-
-            var constantProvider = new MagicConstantProvider();
-            long magicConstant = constantProvider.CalculateMagicConstant(3);
-            Assert.IsTrue(magicConstant > 0);
-            Assert.AreEqual(magicConstant, 15);
-
-            magicConstant = constantProvider.CalculateMagicConstant(2);
-            Assert.IsTrue(magicConstant > 0);
-            Assert.AreEqual(magicConstant, 5);
-
-
-            magicConstant = constantProvider.CalculateMagicConstant(4);
-            Assert.IsTrue(magicConstant > 0);
-            Assert.AreEqual(magicConstant, 34);
-
-
-            magicConstant = constantProvider.CalculateMagicConstant(122001);
-            Assert.IsTrue(magicConstant > 0);
-            Assert.AreEqual(magicConstant, 907946326244001);
-        }
-
-    }
 }
--- MagicSquares.BL/OddMagicSquareProvider.cs
+++ MagicSquares.BL/OddMagicSquareProvider.cs
@@ -37,6 +37,5 @@ namespace MagicSquares.BL
 
             return squareArray;
         }
-
     }
 }
